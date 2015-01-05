b9f849155b38c4e7b5559cfd0771b449bdbefff6
Andrew Spiering <aspiering@gmail.com>
2015-01-04 20:30:04 -0800

    - adding windows tracking for memory - Fixed some issues with the posix tracking and added several more meaningful values!

--- memory.go
+++ memory.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"io/ioutil"
-	"strconv"
-	"strings"
-	"time"
-)
-
 const MemoryModuleName = "memory"
 
 type MemoryInputModule struct{}
@@ -21,55 +14,4 @@ func (m *MemoryInputModule) Init(config *Config, moduleConfig *ModuleConfig) err
 
 func (m *MemoryInputModule) TearDown() error {
 	return nil
-}
-
-func (m *MemoryInputModule) GetMetrics() ([]Metric, error) {
-	metrics := make([]Metric, 0, 50)
-	now := time.Now()
-
-	meminfo, err := ParseMeminfo("/proc/meminfo")
-	if err != nil {
-		return nil, err
-	}
-
-	used := meminfo["MemTotal"] - (meminfo["MemFree"] + meminfo["Buffers"] + meminfo["Cached"] + meminfo["Slab"])
-
-	metrics = append(metrics, Metric{module: m.Name(), name: "Used", value: used, timestamp: now})
-	metrics = append(metrics, Metric{module: m.Name(), name: "Free", value: meminfo["MemFree"], timestamp: now})
-	metrics = append(metrics, Metric{module: m.Name(), name: "Buffered", value: meminfo["Buffers"], timestamp: now})
-	metrics = append(metrics, Metric{module: m.Name(), name: "Cached", value: meminfo["Cached"], timestamp: now})
-	metrics = append(metrics, Metric{module: m.Name(), name: "SlabReclaimable", value: meminfo["SReclaimable"], timestamp: now})
-	metrics = append(metrics, Metric{module: m.Name(), name: "SlabUnreclaimable", value: meminfo["SUnreclaim"], timestamp: now})
-
-	return metrics, nil
-}
-
-func ParseMeminfo(path string) (map[string]float64, error) {
-	meminfo := make(map[string]float64)
-
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	content := string(b)
-	lines := strings.Split(content, "\n")
-
-	for _, line := range lines {
-		fields := strings.Fields(line)
-		if len(fields) < 2 {
-			continue
-		}
-
-		name := strings.Split(fields[0], ":")[0]
-		value, err := strconv.ParseFloat(fields[1], 64)
-		if err == nil {
-			if len(fields) > 2 && fields[2] == "kB" {
-				value = value * 1024
-			}
-
-			meminfo[name] = value
-		}
-	}
-
-	return meminfo, nil
 }
---
+++ memory_posix.go
@@ -0,0 +1,81 @@
+// +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris
+
+package main
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func (m *MemoryInputModule) GetMetrics() ([]Metric, error) {
+	metrics := make([]Metric, 0, 50)
+	now := time.Now()
+
+	meminfo, err := ParseMeminfo("/proc/meminfo")
+	if err != nil {
+		return nil, err
+	}
+
+	metrics = append(metrics, Metric{module: m.Name(), name: "Total", value: meminfo["MemTotal"], timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "Free", value: meminfo["MemFree"], timestamp: now})
+
+	used := meminfo["MemTotal"] - (meminfo["MemFree"])
+
+	metrics = append(metrics, Metric{module: m.Name(), name: "Used", value: used, timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "Cached", value: meminfo["Cached"], timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "Buffer", value: meminfo["Buffers"], timestamp: now})
+
+	bcTotal := meminfo["Cached"] + meminfo["Buffers"]
+	bcUsed := used - bcTotal
+	bcFree := meminfo["MemFree"] + bcTotal
+
+	metrics = append(metrics, Metric{module: m.Name(), name: "BufferCacheTotal", value: bcTotal, timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "BufferCacheUser", value: bcUsed, timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "BufferCacheFree", value: bcFree, timestamp: now})
+
+	metrics = append(metrics, Metric{module: m.Name(), name: "SwapTotal", value: meminfo["SwapTotal"], timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "SwapFree", value: meminfo["SwapFree"], timestamp: now})
+
+	metrics = append(metrics, Metric{module: m.Name(), name: "HighTotal", value: meminfo["HighTotal"], timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "HighFree", value: meminfo["HighFree"], timestamp: now})
+
+	metrics = append(metrics, Metric{module: m.Name(), name: "LowTotal", value: meminfo["LowTotal"], timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "LowFree", value: meminfo["LowFree"], timestamp: now})
+
+	metrics = append(metrics, Metric{module: m.Name(), name: "SlabReclaimable", value: meminfo["SReclaimable"], timestamp: now})
+	metrics = append(metrics, Metric{module: m.Name(), name: "SlabUnreclaimable", value: meminfo["SUnreclaim"], timestamp: now})
+
+	return metrics, nil
+}
+
+func ParseMeminfo(path string) (map[string]float64, error) {
+	meminfo := make(map[string]float64)
+
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	content := string(b)
+	lines := strings.Split(content, "\n")
+
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			continue
+		}
+
+		name := strings.Split(fields[0], ":")[0]
+		value, err := strconv.ParseFloat(fields[1], 64)
+		if err == nil {
+			if len(fields) > 2 && fields[2] == "kB" {
+				value = value * 1024
+			}
+
+			meminfo[name] = value
+		}
+	}
+
+	return meminfo, nil
+}
